e833ea13353c70a30d5c4e5defbce6572587b8c7
Joe Cowton <joe.cowton@guardian.co.uk>
2021-08-10 10:32:44 +0100

    LIVE-2906: add shareIcon type

--- src/communications.ts
+++ src/communications.ts
@@ -1,101 +1,117 @@
 // https://github.com/react-native-webview/react-native-webview/blob/master/src/WebViewTypes.ts#L963
 declare global {
-  interface Window {
-    ReactNativeWebView?: {
-      postMessage: (data: string) => void;
-    };
+	interface Window {
+		ReactNativeWebView?: {
+			postMessage: (data: string) => void;
+		};
 
-    pingEditionsRendering?: (message: Message) => void;
-    pingEditionsRenderingJsString?: (message: Message) => string;
-  }
+		pingEditionsRendering?: (message: Message) => void;
+		pingEditionsRenderingJsString?: (message: Message) => string;
+	}
 }
 
 export enum MessageKind {
-  Share = "Share",
-  Platform = "Platform",
-  PlatformQuery = "PlatformQuery",
-  Lightbox = "Lightbox",
+	Share = "Share",
+	Platform = "Platform",
+	PlatformQuery = "PlatformQuery",
+	Lightbox = "Lightbox",
+	ShareIcon = "ShareIcon",
 }
 
 export type ShareMessage = {
-  kind: MessageKind.Share;
+	kind: MessageKind.Share;
 };
 
 export type PlatformMessage = {
-  kind: MessageKind.Platform;
-  value: Platform;
+	kind: MessageKind.Platform;
+	value: Platform;
 };
 
 export type PlatformQueryMessage = {
-  kind: MessageKind.PlatformQuery;
+	kind: MessageKind.PlatformQuery;
 };
 
 export type LightboxMessage = {
-  kind: MessageKind.Lightbox;
-  index: number;
-  isMainImage: boolean;
+	kind: MessageKind.Lightbox;
+	index: number;
+	isMainImage: boolean;
+};
+
+export type ShareIconMessage = {
+	kind: MessageKind.ShareIcon;
+	value: string;
 };
 
 export type Message =
-  | ShareMessage
-  | PlatformMessage
-  | PlatformQueryMessage
-  | LightboxMessage;
+	| ShareMessage
+	| PlatformMessage
+	| PlatformQueryMessage
+	| LightboxMessage
+	| ShareIconMessage;
 
 export enum Platform {
-  IOS = "IOS",
-  Android = "Android",
+	IOS = "IOS",
+	Android = "Android",
 }
 
 export const isPlatformMessageEvent = (
-  customEvent: CustomEventInit<Message>
+	customEvent: CustomEventInit<Message>
 ): customEvent is CustomEvent<PlatformMessage> => {
-  if (
-    customEvent.detail?.kind === MessageKind.Platform &&
-    customEvent.detail.value in Platform
-  ) {
-    return true;
-  }
-  return false;
+	if (
+		customEvent.detail?.kind === MessageKind.Platform &&
+		customEvent.detail.value in Platform
+	) {
+		return true;
+	}
+	return false;
+};
+
+export const isShareIconMessageEvent = (
+	event: CustomEventInit<Message>
+): boolean => {
+	if (event.detail?.kind === MessageKind.ShareIcon) {
+		return true;
+	}
+	return false;
 };
 
 const prettyLog = (logMessage: string, data: string | Message): void => {
-  const parsedData: unknown =
-    typeof data === "string" ? JSON.parse(data) : data;
-  console.log(
-    `%c${logMessage}
+	const parsedData: unknown =
+		typeof data === "string" ? JSON.parse(data) : data;
+	console.log(
+		`%c${logMessage}
 ${JSON.stringify(parsedData, null, 2)}`,
-    "color: #45969b;"
-  );
+		"color: #45969b;"
+	);
 };
 
 export const pingEditionsNative = (message: Message): void => {
-  const serializedMessage = JSON.stringify(message);
+	const serializedMessage = JSON.stringify(message);
 
-  if (process.env.NODE_ENV === "development") {
-    prettyLog(
-      "🎨 => 📲  Pinging Editions native from Editions Rendering with message:",
-      serializedMessage
-    );
-  }
+	if (process.env.NODE_ENV === "development") {
+		prettyLog(
+			"🎨 => 📲  Pinging Editions native from Editions Rendering with message:",
+			serializedMessage
+		);
+	}
 
-  window.ReactNativeWebView?.postMessage(serializedMessage);
+	window.ReactNativeWebView?.postMessage(serializedMessage);
 };
 
 const pingEditionsRendering = (message: Message): void => {
-  if (process.env.NODE_ENV === "development") {
-    prettyLog(
-      "📱 => 🎨  Pinging Editions Rendering from Editions native with message: ",
-      message
-    );
-  }
+	if (process.env.NODE_ENV === "development") {
+		prettyLog(
+			"📱 => 🎨  Pinging Editions Rendering from Editions native with message: ",
+			message
+		);
+	}
 
-  const customEvent = new CustomEvent("editionsPing", { detail: message });
-  document.dispatchEvent(customEvent);
+	const customEvent = new CustomEvent("editionsPing", { detail: message });
+	document.dispatchEvent(customEvent);
 };
 
 export const pingEditionsRenderingJsString = (message: Message): string => {
-  return `
+	return `
         try {
             window.pingEditionsRendering(${JSON.stringify(message)})
         } catch {
@@ -105,5 +121,5 @@ export const pingEditionsRenderingJsString = (message: Message): string => {
 };
 
 export const initPingEditionsRendering = (): void => {
-  window.pingEditionsRendering = pingEditionsRendering;
+	window.pingEditionsRendering = pingEditionsRendering;
 };
